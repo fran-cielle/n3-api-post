0dd9d5a31091ac5bb68e878c7a96a74fdfbcedcd
fran-cielle <nasc.francielle@gmail.com>
2019-11-05 22:16:49 -0300

    inserindo body parse para incluir item no obj

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -1,18 +1,18 @@
-const contatos = require("../model/contatos");
+const model = require("../model/contatos");
 
 const getAll = (request, response) => {
   console.log(request.url);
-  response.status(200).send(contatos.model);
+  response.status(200).send(contatos.agenda);
 };
 
-const getById = (request, response) => {
-  const id = request.params.id;
-  response.status(200).send(contatos.find(tarefa => tarefa.id == id));
-};
-
+const add = (request, response) =>{
+ 
+   model.agenda.contatos.push(request.body)//push é um método de uma array fornece um objeto que tem uma agenda dentro
+  response.status(200).send()
+}
 
 module.exports = {
   getAll,
-  getById
+  add
 }
 
--- servidor-contato/src/routes/contatosRoute.js
+++ servidor-contato/src/routes/contatosRoute.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
 const controller = require("../controller/contatosController")
+const bodyParser = require("body-parser")
 
 router.get("/", controller.getAll)
-router.get("/:id", controller.getById)
+router.post("/criar",bodyParser.json(),controller.add)//o POST sempre cria (ele é capaz de criar) coisas o put é mais utilizado para edição
 
 module.exports = router
