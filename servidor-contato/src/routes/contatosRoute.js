6a4a89d69d3053138495737d75e75d416273a41d
Josélia L. Costa <joselialcosta@gmail.com>
2019-11-07 17:39:25 -0300

    Evita salvar contatos já existentes e não salva se os parametros não estiverem corretos

--- servidor-contato/src/routes/contatosRoute.js
+++ servidor-contato/src/routes/contatosRoute.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const router = express.Router()
+const bodyParser = require("body-parser")
 const controller = require("../controller/contatosController")
 const bodyParser = require("body-parser")
 
 router.get("/", controller.getAll)
+<<<<<<< HEAD
 router.post("/criar",bodyParser.json(),controller.add)//o POST sempre cria (ele é capaz de criar) coisas o put é mais utilizado para edição
+=======
+router.post("/criar", bodyParser.json(), controller.add)
+>>>>>>> Evita salvar contatos já existentes e não salva se os parametros não estiverem corretos
 
 module.exports = router
