c21805e81aa5f16082c3f66a5da7c2e4507c2cda
fran-cielle <nasc.francielle@gmail.com>
2019-11-07 22:22:41 -0300

    atualizacoes dia 07-11

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -2,14 +2,26 @@ const model = require("../model/contatos");
 
 const getAll = (request, response) => {
   console.log(request.url);
-  response.status(200).send(contatos.agenda);
+  response.status(200).send(model.agenda);
 };
 
 const add = (request, response) =>{
- 
-   model.agenda.contatos.push(request.body)//push é um método de uma array fornece um objeto que tem uma agenda dentro
+ let coletaDados = request.body
+  coletaDados.id = Math.random().toString(36).substr(-8)//para gerar um numero "automatico" no ID
+
+  model.agenda.contatos.forEach(coletaDados =>{
+    if ((dado == coletaDados.nome) && (dado == coletaDados.dataNascimento)) {
+      response.status(400).send()
+    }else{
+      return dado;
+    }
+  })
+  }    
+  
+  let dados = model.agenda.contatos.push(coletaDados)//push é um método de uma array fornece um objeto que tem uma agenda dentro
   response.status(200).send()
-}
+};
+
 
 module.exports = {
   getAll,
