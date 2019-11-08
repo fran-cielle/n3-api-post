c119be45b32553d7c0c4d58d7939d1076659adde
fran-cielle <nasc.francielle@gmail.com>
2019-11-08 19:34:16 -0300

    correção de conflito do merge

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -5,22 +5,22 @@ const getAll = (request, response) => {
   response.status(200).send(model.agenda);
 };
 
-const add = (request, response) =>{
- let coletaDados = request.body
+const add = (request, response) => {
+  let coletaDados = request.body
   coletaDados.id = Math.random().toString(36).substr(-8)//para gerar um numero "automatico" no ID
 
-  model.agenda.contatos.forEach(coletaDados =>{
+  model.agenda.contatos.forEach(coletaDados => {
     if ((dado == coletaDados.nome) && (dado == coletaDados.dataNascimento)) {
       response.status(400).send()
-    }else{
+    } else {
       return dado;
     }
   })
-  }    
-  
+
   let dados = model.agenda.contatos.push(coletaDados)//push é um método de uma array fornece um objeto que tem uma agenda dentro
   response.status(200).send()
-};
+
+}
 
 
 module.exports = {
